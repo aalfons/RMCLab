1169221237aee09566abb5c41953c96cf407ec5d
Andreas Alfons <alfons@ese.eur.nl>
2024-06-18 10:41:31 +0200

    functions for validation splits now use object-oriented implementation

--- R/splits.R
+++ R/splits.R
@@ -3,23 +3,56 @@
 #         Erasmus Universiteit Rotterdam
 # **************************************
 
-## function to draw indices for holdout validation
+## control object for holdout validation
 #' @export
-holdout <- function(indices, pct = 0.1, R = 10) {
+holdout_control <- function(pct = 0.1, R = 10L) {
+  if (pct <= 0 || pct >= 1) {
+    stop("the percentage of holdout elements must be in the interval (0, 1)")
+  }
+  if (R < 1L) stop("holdout validation requires at least 1 replication")
+  control <- list(pct = pct, R = R)
+  class(control) <- "holdout_control"
+  control
+}
+
+## control object for cross-validation
+#' @export
+fold_control <- function(K = 5L) {
+  if (K < 2L) stop("cross-validation requires at least 2 folds")
+  control <- list(K = K)
+  class(control) <- "fold_control"
+  control
+}
+
+
+## generic function for validation splits
+#' @export
+splits <- function(indices, control) UseMethod("splits", control)
+
+## method to draw indices for holdout validation
+#' @export
+splits.holdout_control <- function(indices, control) {
+  # initializations
   n <- length(indices)
+  pct <- control$pct
+  R <- control$R
+  # check if enough indices have been supplied
   min <- ceiling(0.5000000001/pct) # avoid issues with floating point arithmetic
   if (n < min) {
     stop(sprintf("'indices' should contain at least %d elements", min))
   }
+  # draw indices of holdout set
   m <- round(pct * n)
   replicate(R, sample(indices, size = m), simplify = FALSE)
 }
 
-## function to draw folds of indices for K-fold cross-validation
+## method to draw folds of indices for K-fold cross-validation
 #' @export
-cv_folds <- function(indices, K = 5L) {
+splits.fold_control <- function(indices, control) {
+  # initialization
   n <- length(indices)
-  if (K < 2L) stop("cross-validation requires at least 2 folds")
+  K <- control$K
+  # check if enough indices have been supplied
   if (n < K) stop(sprintf("'indices' should contain at least %d elements", K))
   # permute observations
   indices <- sample(indices)
@@ -30,3 +63,46 @@ cv_folds <- function(indices, K = 5L) {
   names(folds) <- NULL
   folds
 }
+  
+
+## wrapper function to draw indices for holdout validation
+#' @export
+holdout <- function(indices, pct = 0.1, R = 10) {
+  control <- holdout_control(pct = pct, R = R)
+  splits(indices, control)
+}
+
+
+## wrapper function to draw folds of indices for K-fold cross-validation
+#' @export
+cv_folds <- function(indices, K = 5L) {
+  control <- fold_control(K = K)
+  splits(indices, control)
+}
+
+
+# ## function to draw indices for holdout validation
+# holdout <- function(indices, pct = 0.1, R = 10) {
+#   n <- length(indices)
+#   min <- ceiling(0.5000000001/pct) # avoid issues with floating point arithmetic
+#   if (n < min) {
+#     stop(sprintf("'indices' should contain at least %d elements", min))
+#   }
+#   m <- round(pct * n)
+#   replicate(R, sample(indices, size = m), simplify = FALSE)
+# }
+# 
+# ## function to draw folds of indices for K-fold cross-validation
+# cv_folds <- function(indices, K = 5L) {
+#   n <- length(indices)
+#   if (K < 2L) stop("cross-validation requires at least 2 folds")
+#   if (n < K) stop(sprintf("'indices' should contain at least %d elements", K))
+#   # permute observations
+#   indices <- sample(indices)
+#   # assign a block to each observation
+#   blocks <- rep(seq_len(K), length.out = n)
+#   # split the permuted observations according to the block they belong to
+#   folds <- split(indices, blocks)
+#   names(folds) <- NULL
+#   folds
+# }
